2d1cd97084dc5d1db59cb3c69cae6b62745b8f47
tmuneeb <131722044+tmuneeb@users.noreply.github.com>
2023-05-21 03:56:35 +0500

    Create headless.js
    headless

---
+++ headless.js
@@ -0,0 +1,64 @@
+const { Client } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+const puppeteer = require('puppeteer-extra');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+
+puppeteer.use(StealthPlugin());
+
+const client = new Client({
+  puppeteer: puppeteer,
+  authOptions: {
+    authType: 'local',
+    disableSpins: true,
+  },
+  headless: true,
+  args: [
+    '--no-sandbox',
+    '--disable-setuid-sandbox',
+    '--disable-dev-shm-usage',
+    '--single-process',
+  ],
+  ignoreDefaultArgs: ['--disable-extensions'],
+});
+
+const messagePrefix = "...";
+
+async function runBard(msg) {
+  const { init, askAI, Chat } = await import("bard-ai");
+
+  await init("WwgQ-vtIJ9mBP_llStJ2rjZwS_2pnHX-bkYS-qFhytt_nN5e31crTMrg7sI0H9SRZC75cQ.");
+  let myConversation = new Chat();
+  let response = await myConversation.ask(msg);
+  console.log(response);
+  return response;
+}
+
+function displayQRCode(qr) {
+  // Display the QR code in the terminal
+  console.log('Scan the QR code below:');
+  qrcode.generate(qr, { small: true });
+}
+
+async function initializeClient() {
+  client.on('qr', (qr) => {
+    displayQRCode(qr);
+  });
+
+  client.on('ready', () => {
+    console.log('Client is ready!');
+  });
+
+  client.on('message', async (msg) => {
+    if (msg.body.includes(messagePrefix)) {
+      const stripped = msg.body.replace(messagePrefix, '');
+      const response = await runBard(stripped);
+      msg.reply(response);
+    }
+  });
+
+  await client.initialize();
+}
+
+initializeClient().catch((error) => {
+  console.error(error);
+});
